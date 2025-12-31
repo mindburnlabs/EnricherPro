34da705893fa450d1f5eac9eafcec29e5d50ac7b
mindburnlabs <login@mindburnlabs.com>
2025-12-31 19:06:10 +0200

    fix: Resolve TS import errors and finalize localization integration

--- src/repositories/ClaimsRepository.ts
+++ src/repositories/ClaimsRepository.ts
@@ -1,5 +1,5 @@
-import { db } from "../../db/index.js";
-import { claims } from "../../db/schema.js";
+import { db } from "../db/index.js";
+import { claims } from "../db/schema.js";
 import { eq } from "drizzle-orm";
 
 export class ClaimsRepository {
--- src/repositories/SourceDocumentRepository.ts
+++ src/repositories/SourceDocumentRepository.ts
@@ -1,5 +1,5 @@
-import { db } from "../../db/index.js";
-import { sourceDocuments } from "../../db/schema.js";
+import { db } from "../db/index.js";
+import { sourceDocuments } from "../db/schema.js";
 import { eq } from "drizzle-orm";
 import crypto from "crypto";
 
